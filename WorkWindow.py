7fd232201a67a286404ec369403cc04724be1eac
Aniyar <45550980+Aniyar@users.noreply.github.com>
2018-12-09 17:55:22 +0600

    some errors fixed
    division by zero
    kb added
    clear button added
    slightly different interface

--- WorkWindow.py
+++ WorkWindow.py
@@ -9,10 +9,18 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from numpy import arange
 import pyqtgraph as pg
-from math import *
-
+from math import sin, cos, tan
+from keyboard import Ui_MainWindow
 
 class Ui_WorkWindow(object):
+
+    def axes(self):
+        self.graphicsView.plot([i for i in arange(-self.k, self.k + 0.1, 0.1)],
+                               [0 for x in arange(-self.k, self.k + 0.1, 0.1)],
+                               pen='w')
+        self.graphicsView.plot([0 for i in arange(-self.k, self.k + 0.1, 0.1)],
+                               [x for x in arange(-self.k, self.k + 0.1, 0.1)],
+                               pen='w')
 
     def color(self):
         color = QtWidgets.QColorDialog.getColor()
@@ -24,26 +32,59 @@ class Ui_WorkWindow(object):
 
     def run(self):
         self.k = self.sl.value()
+        self.graphicsView.setRange(yRange=[-self.k, self.k + 0.1])
+        self.graphicsView.setRange(xRange=[-self.k, self.k + 0.1])
+        self.axes()
         self.func = self.functionlineEdit.text()
-        self.graphicsView.plot([i for i in arange(-self.k, self.k, 0.1)],
-                    [eval(self.func) for x in arange(-self.k, self.k, 0.1)],
+        self.graphicsView.plot([i for i in arange(-self.k, self.k + 0.1, 0.1)],
+                    [self.check(x, self.func) for x in arange(-self.k, self.k + 0.1, 0.1)],
                     pen=self.clr)
         word = '<span style=\" color: %s;\">%s</span>' % (self.clr, self.func)
+        self.func_lst.append((self.func, self.clr))
         self.textBrowser.append(word)
 
-    def setupUi(self, WorkWindow):
+    def scale_change(self):
+        self.k = self.sl.value()
+        self.graphicsView.setRange(yRange=[-self.k, self.k + 0.1])
+        self.graphicsView.setRange(xRange=[-self.k, self.k + 0.1])
+        self.axes()
+        for i in self.func_lst:
+            self.graphicsView.plot([j for j in arange(-self.k, self.k + 0.1, 0.1)],
+                                   [self.check(x, i[0]) for x in arange(-self.k, self.k + 0.1, 0.1)],
+                                   pen=i[1])
 
+    def check(self, x, func):
+        try:
+            y = eval(func)
+        except Exception:
+            return None
+        return y
+
+    def clearall(self):
+        self.graphicsView.clear()
+        self.textBrowser.clear()
+        self.func_lst = []
+
+    def open_kb(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.window)
+        self.window.show()
+
+    def setupUi(self, WorkWindow):
+        self.func_lst = []
         WorkWindow.setObjectName("WorkWindow")
-        WorkWindow.resize(660, 640)
+        WorkWindow.resize(658, 595)
+
         self.centralwidget = QtWidgets.QWidget(WorkWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(510, 180, 141, 361))
+        self.textBrowser.setGeometry(QtCore.QRect(510, 180, 141, 291))
         self.textBrowser.setObjectName("textBrowser")
 
         self.sl = QtWidgets.QSlider(self.centralwidget)
-        self.sl.setGeometry(QtCore.QRect(90, 100, 181, 19))
+        self.sl.setGeometry(QtCore.QRect(510, 110, 121, 20))
         self.sl.setOrientation(QtCore.Qt.Horizontal)
         self.sl.setObjectName("sl")
         self.sl.setMinimum(1)
@@ -51,64 +92,76 @@ class Ui_WorkWindow(object):
         self.sl.setValue(1)
         self.sl.setTickPosition(QtWidgets.QSlider.TicksBelow)
         self.sl.setTickInterval(1)
-        self.k = self.sl.value()
+        self.sl.valueChanged.connect(self.scale_change)
 
         self.DrawButton = QtWidgets.QPushButton(self.centralwidget)
-        self.DrawButton.setGeometry(QtCore.QRect(420, 80, 121, 31))
+        self.DrawButton.setGeometry(QtCore.QRect(460, 30, 121, 31))
         self.DrawButton.setObjectName("DrawButton")
         self.DrawButton.clicked.connect(self.run)
 
         self.FuctionsLabel = QtWidgets.QLabel(self.centralwidget)
         self.FuctionsLabel.setGeometry(QtCore.QRect(510, 140, 141, 31))
+
         font = QtGui.QFont()
         font.setPointSize(12)
+
         self.FuctionsLabel.setFont(font)
         self.FuctionsLabel.setObjectName("FuctionsLabel")
-
         self.functionlineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.functionlineEdit.setGeometry(QtCore.QRect(90, 40, 181, 21))
         self.functionlineEdit.setObjectName("functionlineEdit")
-        self.func = self.functionlineEdit.text()
 
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(50, 40, 47, 13))
+
         font = QtGui.QFont()
         font.setPointSize(10)
+
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
 
         self.KeyboardButton = QtWidgets.QPushButton(self.centralwidget)
         self.KeyboardButton.setGeometry(QtCore.QRect(280, 40, 31, 21))
         self.KeyboardButton.setObjectName("KeyboardButton")
+        self.KeyboardButton.clicked.connect(self.open_kb)
 
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(30, 10, 260, 27))
+        self.label.setGeometry(QtCore.QRect(30, 10, 239, 27))
+
         font = QtGui.QFont()
         font.setFamily("Angsana New")
         font.setPointSize(14)
+
         self.label.setFont(font)
         self.label.setObjectName("label")
-
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(30, 70, 241, 20))
+        self.label_3.setGeometry(QtCore.QRect(510, 90, 241, 20))
+
         font = QtGui.QFont()
         font.setFamily("AngsanaUPC")
-        font.setPointSize(14)
+        font.setPointSize(12)
+
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
 
         self.ColorButton = QtWidgets.QPushButton(self.centralwidget)
-        self.ColorButton.setGeometry(QtCore.QRect(280, 80, 121, 31))
+        self.ColorButton.setGeometry(QtCore.QRect(330, 30, 121, 31))
         self.ColorButton.setObjectName("ColorButton")
         self.ColorButton.clicked.connect(self.color)
 
         self.graphicsView = pg.PlotWidget(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(10, 130, 491, 461))
+        self.graphicsView.setGeometry(QtCore.QRect(10, 90, 491, 461))
         self.graphicsView.setObjectName("graphicsView")
 
+        self.ClearButton = QtWidgets.QPushButton(self.centralwidget)
+        self.ClearButton.setGeometry(QtCore.QRect(510, 500, 121, 31))
+        self.ClearButton.setObjectName("ClearButton")
+        self.ClearButton.clicked.connect(self.clearall)
+
         WorkWindow.setCentralWidget(self.centralwidget)
+
         self.menubar = QtWidgets.QMenuBar(WorkWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 660, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 658, 21))
         self.menubar.setObjectName("menubar")
         WorkWindow.setMenuBar(self.menubar)
 
@@ -129,6 +182,7 @@ class Ui_WorkWindow(object):
         self.label.setText(_translate("WorkWindow", "Введите значение функции:"))
         self.label_3.setText(_translate("WorkWindow", "Укажите масштаб:"))
         self.ColorButton.setText(_translate("WorkWindow", "Выберите цвет"))
+        self.ClearButton.setText(_translate("WorkWindow", "Стереть все"))
 
 
 if __name__ == "__main__":
@@ -138,4 +192,3 @@ if __name__ == "__main__":
     ui.setupUi(workWindow)
     workWindow.show()
     sys.exit(app.exec())
-
