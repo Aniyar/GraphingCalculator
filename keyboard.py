75579ac58287f7b9076b2e80a52ba0754dbd3bde
Aniyar <45550980+Aniyar@users.noreply.github.com>
2018-12-13 19:33:40 +0600

    complete

--- keyboard.py
+++ keyboard.py
@@ -8,18 +8,28 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-value = ''
 
+class Ui_MainWindow(QtWidgets.QMainWindow):
 
-class Ui_MainWindow(object):
+    def __init__(self, kb_done):
+        super().__init__()
+        self.setupUi(self)
+        self.kb_done = kb_done
 
-    def write(self, btn):
-        self.leval += btn.text()
+    def write(self):
+        if self.sender() == self.degree_btn:
+            self.leval += '**'
+        elif self.sender() == self.cot_btn:
+            self.leval += 'tan**-1'
+        else:
+            self.leval += self.sender().text()
         self.lineEdit.setText(self.leval)
 
     def done(self):
-        global value
-        value = self.leval
+        self.kb_done()
+
+    def get_value(self):
+        return self.leval
 
     def backspace(self):
         self.leval = self.leval[:-1]
@@ -35,8 +45,18 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(370, 354)
 
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(40, 10, 301, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
 
         self.Backspace = QtWidgets.QPushButton(self.centralwidget)
         self.Backspace.setGeometry(QtCore.QRect(10, 60, 51, 51))
@@ -51,160 +71,147 @@ class Ui_MainWindow(object):
         self.plusminus_btn = QtWidgets.QPushButton(self.centralwidget)
         self.plusminus_btn.setGeometry(QtCore.QRect(160, 60, 51, 51))
         self.plusminus_btn.setObjectName("plusminus_btn")
-        self.plusminus_btn.clicked.connect(self.write(self.plusminus_btn))
+        self.plusminus_btn.clicked.connect(self.write)
 
         self.root_btn = QtWidgets.QPushButton(self.centralwidget)
         self.root_btn.setGeometry(QtCore.QRect(210, 60, 51, 51))
         self.root_btn.setObjectName("root_btn")
-        self.root_btn.clicked.connect(self.write(self.root_btn))
+        self.root_btn.clicked.connect(self.write)
 
         self.seven_btn = QtWidgets.QPushButton(self.centralwidget)
         self.seven_btn.setGeometry(QtCore.QRect(10, 110, 51, 51))
         self.seven_btn.setObjectName("seven_btn")
-        self.seven_btn.clicked.connect(self.write(self.seven_btn))
+        self.seven_btn.clicked.connect(self.write)
 
         self.eight_btn = QtWidgets.QPushButton(self.centralwidget)
         self.eight_btn.setGeometry(QtCore.QRect(60, 110, 51, 51))
         self.eight_btn.setObjectName("eight_btn")
-        self.eight_btn.clicked.connect(self.write(self.eight_btn))
+        self.eight_btn.clicked.connect(self.write)
 
         self.nine_btn = QtWidgets.QPushButton(self.centralwidget)
         self.nine_btn.setGeometry(QtCore.QRect(110, 110, 51, 51))
         self.nine_btn.setObjectName("nine_btn")
-        self.nine_btn.clicked.connect(self.write(self.nine_btn))
+        self.nine_btn.clicked.connect(self.write)
 
         self.division_btn = QtWidgets.QPushButton(self.centralwidget)
         self.division_btn.setGeometry(QtCore.QRect(160, 110, 51, 51))
         self.division_btn.setObjectName("division_btn")
-        self.division_btn.clicked.connect(self.write(self.division_btn))
+        self.division_btn.clicked.connect(self.write)
 
         self.degree_btn = QtWidgets.QPushButton(self.centralwidget)
         self.degree_btn.setGeometry(QtCore.QRect(210, 110, 51, 51))
         self.degree_btn.setObjectName("degree_btn")
-        self.degree_btn.clicked.connect(self.write(self.degree_btn))
+        self.degree_btn.clicked.connect(self.write)
 
         self.four_btn = QtWidgets.QPushButton(self.centralwidget)
         self.four_btn.setGeometry(QtCore.QRect(10, 160, 51, 51))
         self.four_btn.setObjectName("four_btn")
-        self.four_btn.clicked.connect(self.write(self.four_btn))
+        self.four_btn.clicked.connect(self.write)
 
         self.five_btn = QtWidgets.QPushButton(self.centralwidget)
         self.five_btn.setGeometry(QtCore.QRect(60, 160, 51, 51))
         self.five_btn.setObjectName("five_btn")
-        self.five_btn.clicked.connect(self.write(self.five_btn))
+        self.five_btn.clicked.connect(self.write)
 
         self.six_btn = QtWidgets.QPushButton(self.centralwidget)
         self.six_btn.setGeometry(QtCore.QRect(110, 160, 51, 51))
         self.six_btn.setObjectName("six_btn")
-        self.six_btn.clicked.connect(self.write(self.six_btn))
+        self.six_btn.clicked.connect(self.write)
 
         self.multi_btn = QtWidgets.QPushButton(self.centralwidget)
         self.multi_btn.setGeometry(QtCore.QRect(160, 160, 51, 51))
         self.multi_btn.setObjectName("multi_btn")
-        self.multi_btn.clicked.connect(self.write(self.multi_btn))
+        self.multi_btn.clicked.connect(self.write)
 
         self.opbracket_btn = QtWidgets.QPushButton(self.centralwidget)
         self.opbracket_btn.setGeometry(QtCore.QRect(210, 210, 51, 51))
         self.opbracket_btn.setObjectName("opbracket_btn")
-        self.opbracket_btn.clicked.connect(self.write(self.opbracket_btn))
+        self.opbracket_btn.clicked.connect(self.write)
 
         self.one_btn = QtWidgets.QPushButton(self.centralwidget)
         self.one_btn.setGeometry(QtCore.QRect(10, 210, 51, 51))
         self.one_btn.setObjectName("one_btn")
-        self.one_btn.clicked.connect(self.write(self.one_btn))
+        self.one_btn.clicked.connect(self.write)
 
         self.two_btn = QtWidgets.QPushButton(self.centralwidget)
         self.two_btn.setGeometry(QtCore.QRect(60, 210, 51, 51))
         self.two_btn.setObjectName("two_btn")
-        self.two_btn.clicked.connect(self.write(self.two_btn))
+        self.two_btn.clicked.connect(self.write)
 
         self.three_btn = QtWidgets.QPushButton(self.centralwidget)
         self.three_btn.setGeometry(QtCore.QRect(110, 210, 51, 51))
         self.three_btn.setObjectName("three_btn")
-        self.three_btn.clicked.connect(self.write(self.three_btn))
+        self.three_btn.clicked.connect(self.write)
 
         self.minus_btn = QtWidgets.QPushButton(self.centralwidget)
         self.minus_btn.setGeometry(QtCore.QRect(160, 210, 51, 51))
         self.minus_btn.setObjectName("minus_btn")
-        self.minus_btn.clicked.connect(self.write(self.minus_btn))
+        self.minus_btn.clicked.connect(self.write)
 
         self.clbracket_btn = QtWidgets.QPushButton(self.centralwidget)
         self.clbracket_btn.setGeometry(QtCore.QRect(210, 160, 51, 51))
         self.clbracket_btn.setObjectName("clbracket_btn")
-        self.clbracket_btn.clicked.connect(self.write(self.clbracket_btn))
+        self.clbracket_btn.clicked.connect(self.write)
 
         self.zero_btn = QtWidgets.QPushButton(self.centralwidget)
         self.zero_btn.setGeometry(QtCore.QRect(10, 260, 101, 51))
         self.zero_btn.setObjectName("zero_btn")
-        self.zero_btn.clicked.connect(self.write(self.zero_btn))
+        self.zero_btn.clicked.connect(self.write)
 
         self.sin_btn = QtWidgets.QPushButton(self.centralwidget)
         self.sin_btn.setGeometry(QtCore.QRect(260, 60, 51, 51))
         self.sin_btn.setObjectName("sin_btn")
-        self.sin_btn.clicked.connect(self.write(self.sin_btn))
+        self.sin_btn.clicked.connect(self.write)
 
         self.dot_btn = QtWidgets.QPushButton(self.centralwidget)
         self.dot_btn.setGeometry(QtCore.QRect(110, 260, 51, 51))
         self.dot_btn.setObjectName("dot_btn")
-        self.dot_btn.clicked.connect(self.write(self.dot_btn))
+        self.dot_btn.clicked.connect(self.write)
 
         self.plus_btn = QtWidgets.QPushButton(self.centralwidget)
         self.plus_btn.setGeometry(QtCore.QRect(160, 260, 51, 51))
         self.plus_btn.setObjectName("plus_btn")
-        self.plus_btn.clicked.connect(self.write(self.plus_btn))
+        self.plus_btn.clicked.connect(self.write)
 
         self.done_btn = QtWidgets.QPushButton(self.centralwidget)
         self.done_btn.setGeometry(QtCore.QRect(210, 260, 151, 51))
         self.done_btn.setObjectName("done_btn")
         self.done_btn.clicked.connect(self.done)
 
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(40, 10, 301, 41))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.lineEdit.setFont(font)
-        self.lineEdit.setText("")
-        self.lineEdit.setObjectName("lineEdit")
-
         self.cos_btn = QtWidgets.QPushButton(self.centralwidget)
         self.cos_btn.setGeometry(QtCore.QRect(260, 110, 51, 51))
         self.cos_btn.setObjectName("cos_btn")
-        self.cos_btn.clicked.connect(self.write(self.cos_btn))
+        self.cos_btn.clicked.connect(self.write)
 
         self.tan_btn = QtWidgets.QPushButton(self.centralwidget)
         self.tan_btn.setGeometry(QtCore.QRect(260, 160, 51, 51))
         self.tan_btn.setObjectName("tan_btn")
-        self.tan_btn.clicked.connect(self.write(self.tan_btn))
+        self.tan_btn.clicked.connect(self.write)
 
         self.cot_btn = QtWidgets.QPushButton(self.centralwidget)
         self.cot_btn.setGeometry(QtCore.QRect(260, 210, 51, 51))
         self.cot_btn.setObjectName("cot_btn")
-        self.cot_btn.clicked.connect(self.write(self.cot_btn))
+        self.cot_btn.clicked.connect(self.write)
 
         self.arcsin_btn = QtWidgets.QPushButton(self.centralwidget)
         self.arcsin_btn.setGeometry(QtCore.QRect(310, 60, 51, 51))
         self.arcsin_btn.setObjectName("arcsin_btn")
-        self.arcsin_btn.clicked.connect(self.write(self.arcsin_btn))
+        self.arcsin_btn.clicked.connect(self.write)
 
         self.arccos_btn = QtWidgets.QPushButton(self.centralwidget)
         self.arccos_btn.setGeometry(QtCore.QRect(310, 110, 51, 51))
         self.arccos_btn.setObjectName("arccos_btn")
-        self.arccos_btn.clicked.connect(self.write(self.arccos_btn))
+        self.arccos_btn.clicked.connect(self.write)
 
         self.arctan_btn = QtWidgets.QPushButton(self.centralwidget)
         self.arctan_btn.setGeometry(QtCore.QRect(310, 160, 51, 51))
         self.arctan_btn.setObjectName("arctan_btn")
-        self.arctan_btn.clicked.connect(self.write(self.arctan_btn))
-
-        self.arccot_btn = QtWidgets.QPushButton(self.centralwidget)
-        self.arccot_btn.setGeometry(QtCore.QRect(310, 210, 51, 51))
-        self.arccot_btn.setObjectName("arccot_btn")
-        self.arccot_btn.clicked.connect(self.write(self.arccot_btn))
+        self.arctan_btn.clicked.connect(self.write)
 
         self.x_btn = QtWidgets.QPushButton(self.centralwidget)
         self.x_btn.setGeometry(QtCore.QRect(110, 60, 51, 51))
         self.x_btn.setObjectName("x_btn")
-        self.x_btn.clicked.connect(self.write(self.x_btn))
+        self.x_btn.clicked.connect(self.write)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -213,9 +220,9 @@ class Ui_MainWindow(object):
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
 
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -250,16 +257,16 @@ class Ui_MainWindow(object):
         self.cos_btn.setText(_translate("MainWindow", "cos"))
         self.tan_btn.setText(_translate("MainWindow", "tan"))
         self.cot_btn.setText(_translate("MainWindow", "cot"))
-        self.arcsin_btn.setText(_translate("MainWindow", "arcsin"))
-        self.arccos_btn.setText(_translate("MainWindow", "arccos"))
-        self.arctan_btn.setText(_translate("MainWindow", "arctan"))
-        self.arccot_btn.setText(_translate("MainWindow", "arccot"))
+        self.arcsin_btn.setText(_translate("MainWindow", "asin"))
+        self.arccos_btn.setText(_translate("MainWindow", "acos"))
+        self.arctan_btn.setText(_translate("MainWindow", "atan"))
         self.x_btn.setText(_translate("MainWindow", "x"))
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    kb = QtWidgets.QMainWindow()
+    #kb = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
-    ui.setupUi(kb)
-    kb.show()
+    #ui.setupUi(kb)
+    ui.show()
     sys.exit(app.exec())
